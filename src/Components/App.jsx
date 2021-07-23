1b5bbeac22327aa8bc7c0aa669ae24777f2d09cf
DarioMeliaBasus <dario@meliabasus.com>
2021-07-23 12:20:16 +0200

    Add dynamic mapped notes from notes.js

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,12 +5,22 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
 
+import notes from "../notes.js";
+
 
 function App() {
     return(
         <div>
            <Header />
-           <Note />
+           {notes.map(note =>{
+               return(
+                   <Note
+                       key={note.key}
+                       title={note.title}
+                       content={note.content}
+                   />
+               )
+           })}
            <Footer />
            
         </div>
